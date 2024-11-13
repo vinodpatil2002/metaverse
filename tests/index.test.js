3d05aa904722d248fc9254ebdc32759a4152c52a
vinodpatil2002 <vinodnspatil@gmail.com>
2024-11-13 22:34:13 +0530

    Complete tests on signin and started user information tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,6 +6,8 @@ function sum(a, b) {
 
 const BACKEND_URL = "https://localhost:3000";
 
+// descibe blocks
+
 describe("Authentication", () => {
     test("user is able to signup only once", async () => {
         const username = "vinod" + Math.random();
@@ -29,34 +31,75 @@ describe("Authentication", () => {
         expect(updatedResponse.statusCode).toBe(400);
     });
 
-    test('Signup request fails if the username is empty',async()=>{
-        const username = `kirat-${Math.random()}`
-        const password = 123456
+    test("Signup request fails if the username is empty", async () => {
+        const username = `vinod-${Math.random()}`;
+        const password = 123456;
 
-        const response = axios.post(`${BACKEND_URL}/api/v1/signup`,{
-            password
-        })
-        expect(response.statusCode).toBe(400)
+        const response = axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            password,
+        });
+        expect(response.statusCode).toBe(400);
     });
 
-    test('signin succeeds if the username and password are correct', async()=>{
-        const username = `kirat-${Math.random()}`
-        const password = 123456
+    test("signin succeeds if the username and password are correct", async () => {
+        const username = `vinod-${Math.random()}`;
+        const password = 123456;
 
-        await axios.post(`${BACKEND_URL}/api/v1/signup`,{
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
             username,
-            password
+            password,
         });
-        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`,{
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
             username,
-            password
+            password,
         });
 
-        expect(response.statusCode).toBe(200)
-        expect(response.body.token).toBeDefined()
+        expect(response.statusCode).toBe(200);
+        expect(response.body.token).toBeDefined();
+    });
 
-        
-    })
+    test("signin fails if the username and password are incorrect", async () => {
+        const username = `vinod-${Math.random()}`;
+        const password = 123456;
+
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+        });
+
+        const response = await axios.post(
+            `${BACKEND_URL}/api/v1/signin`,
+            async () => {
+                username: "wrong";
+                password;
+            }
+        );
+
+        expect(response.statusCode).toBe(403);
+    });
 });
 
-// descibe blocks
+describe("User Information endpoints", () => {
+    let token = "";
+    beforeAll(async () => {
+        const username = `vinod-${Math.random()}`;
+        const password = 123456;
+        await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password,
+            type: "admin",
+        });
+        const response = await axios.post(
+            `${BACKEND_URL}/api/v1/signin`,
+            async () => {
+                username, password;
+            }
+        );
+
+        token = response.body.token;
+    });
+
+    test1("test1", () => {});
+    test1("test1", () => {});
+    test1("test1", () => {});
+});
